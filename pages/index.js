27c5d9e3d2e144d32b645d22e8b5641300da5fa9
LopeKai <kaiqueoakley35@hotmail.com>
2022-04-30 12:35:08 -0300

    test

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Kaique Lopes. I'm software enginer and a translator (English/Japanese) </p>
+        <p>Hello, I'm Kaique Lopes. I'm Developer Front-End ReactJS. </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
